b3a27cc60895e0acf106449d1fb96a725ae735b3
Stuart Dines <me@stuartdines.com>
2019-08-07 09:42:45 +1000

    Add testing for wget regex

---
+++ measurement/plugins/download_speed/tests/test_measurements.py
@@ -0,0 +1,11 @@
+from measurement.plugins.download_speed.measurements import WGET_OUTPUT_REGEX
+
+
+def test_wget_output_regex_accepts_anticipated_format():
+    anticipated_format = "2019-08-07 09:12:08 (16.7 MB/s) - '/dev/null’ saved [11376]"
+    results = WGET_OUTPUT_REGEX.search(anticipated_format).groupdict()
+    assert results == {
+        "download_rate": "16.7",
+        "download_size": "11376",
+        "download_unit": "MB/s",
+    }
