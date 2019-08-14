8ad4048fe678fb3110bc838d2db4422c6460afb5
Stuart Dines <me@stuartdines.com>
2019-08-14 20:54:21 +1000

    Add support for python 3.5 and 3.8

--- measurement/plugins/download_speed/results.py
+++ measurement/plugins/download_speed/results.py
@@ -1,8 +1,9 @@
 import collections
+import sys
 
 import six
 
-if six.PY3:
+if six.PY3 and not sys.version_info.minor == 5:  # All python 3 expect for 3.5
     from .results_py3 import *
 else:
     DownloadSpeedMeasurementResult = collections.namedtuple(
