a43d009e81e483a0c025b80b1fe3cc4e2d0d26f0
jamesJ <james.johnston@honestybox.com.au>
2019-09-12 13:08:34 +1000

    Find closest server, tests

--- measurement/plugins/download_speed/measurements.py
+++ measurement/plugins/download_speed/measurements.py
@@ -47,11 +47,11 @@ LATENCY_ERRORS = {
 class DownloadSpeedMeasurement(BaseMeasurement):
     """A measurement designed to test download speed."""
 
-    def __init__(self, id, url, count=4, download_timeout=180):
+    def __init__(self, id, urls, count=4, download_timeout=180):
         """Initialisation of a download speed measurement.
 
         :param id: A unique identifier for the measurement.
-        :param url: A URL used to perform the download speed
+        :param urls: A list of URLs used to perform the download speed
         measurement.
         :param count: A positive integer describing the number of
         pings to perform. Defaults to 4.
@@ -59,11 +59,12 @@ class DownloadSpeedMeasurement(BaseMeasurement):
         seconds for the test to last. 0 means no timeout.
         """
         super(DownloadSpeedMeasurement, self).__init__(id=id)
-
-        validated_url = validators.url(url)
-
-        if isinstance(validated_url, ValidationFailure):
-            raise ValueError("`{url}` is not a valid url".format(url=url))
+        if len(urls) < 1:
+            raise ValueError("At least one url must be provided.")
+        for url in urls:
+            validated_url = validators.url(url)
+            if isinstance(validated_url, ValidationFailure):
+                raise ValueError("`{url}` is not a valid url".format(url=url))
 
         if count < 0:
             raise ValueError(
@@ -77,17 +78,32 @@ class DownloadSpeedMeasurement(BaseMeasurement):
                 "integer or `0` to turn off the timeout.".format(count=count)
             )
 
-        self.url = url
+        self.urls = urls
         self.count = count
         self.download_timeout = download_timeout
 
     def measure(self):
         """Perform the measurement."""
-        results = [self._get_wget_results(self.url, self.download_timeout)]
+        initial_latency_results = self._find_least_latent_url(self.urls)
+        least_latent_url = initial_latency_results[0][0]
+        results = [self._get_wget_results(least_latent_url, self.download_timeout)]
         if self.count > 0:
-            host = urlparse(self.url).netloc
+            host = urlparse(least_latent_url).netloc
             results.append(self._get_latency_results(host, self.count))
+
+        results.extend([res for _, res in initial_latency_results])
         return results
+
+    def _find_least_latent_url(self, urls):
+        """
+        Performs a latency test for each specified endpoint
+        Returns a sorted list of LatencyResults, sorted by average latency and None
+        """
+        initial_latency_results = []
+        for url in urls:
+            host = urlparse(url).netloc
+            initial_latency_results.append((url, (self._get_latency_results(host, 2))))
+        return sorted(initial_latency_results, key=lambda x: (x[1].average_latency is None, x[1].average_latency))
 
     def _get_latency_results(self, host, count):
         """Perform the latency measurement."""
--- measurement/plugins/download_speed/tests/test_measurements.py
+++ measurement/plugins/download_speed/tests/test_measurements.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
+from unittest import TestCase, mock
 import six
 
 from measurement.plugins.download_speed.measurements import WGET_OUTPUT_REGEX
+from measurement.plugins.download_speed.measurements import (
+    DownloadSpeedMeasurement,
+)
+from measurement.plugins.download_speed.results import (
+    DownloadSpeedMeasurementResult,
+    LatencyMeasurementResult,
+)
+from measurement.units import NetworkUnit, StorageUnit
 
 
 def test_wget_output_regex_accepts_anticipated_format():
@@ -14,3 +23,102 @@ def test_wget_output_regex_accepts_anticipated_format():
         "download_size": "11376",
         "download_unit": "MB/s",
     }
+
+
+class DownloadSpeedMeasurementCreationTestCase(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+    def test_invalid_hosts(self, *args):
+        self.assertRaises(
+            ValueError,
+            DownloadSpeedMeasurement,
+            "test",
+            ["invalid..host"],
+        )
+
+    def test_invalid_count(self, *args):
+        self.assertRaises(
+            TypeError,
+            DownloadSpeedMeasurement,
+            "test",
+            ["http://validfakeurl.com"],
+            count="invalid-count"
+        )
+
+    def test_invalid_negative_count(self, *args):
+        self.assertRaises(
+            ValueError,
+            DownloadSpeedMeasurement,
+            "test",
+            ["http://validfakeurl.com"],
+            count=-2
+        )
+
+
+class DownloadSpeedMeasurementClosestServerTestCase(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.example_urls = [
+            "http://n1-validfakehost.com",
+            "http://n2-validfakehost.com",
+            "http://n3-validfakehost.com",
+        ]
+        self.measurement = DownloadSpeedMeasurement("test", self.example_urls)
+        print("asdf")
+
+    @mock.patch.object(DownloadSpeedMeasurement, "_get_latency_results")
+    def test_sort_least_latent_url(self, mock_latency_results):
+        results = [
+            LatencyMeasurementResult(
+                id="test",
+                host="n1-validfakehost.com",
+                minimum_latency=None,
+                average_latency=None,
+                maximum_latency=None,
+                median_deviation=None,
+                errors=[],
+            ),
+            LatencyMeasurementResult(
+                id="test",
+                host="n2-validfakehost.com",
+                minimum_latency=None,
+                average_latency=25.0,
+                maximum_latency=None,
+                median_deviation=None,
+                errors=[],
+            ),
+            LatencyMeasurementResult(
+                id="test",
+                host="n3-validfakehost.com",
+                minimum_latency=None,
+                average_latency=999.0,
+                maximum_latency=None,
+                median_deviation=None,
+                errors=[],
+            )
+        ]
+        mock_latency_results.side_effect = results
+        self.assertEqual(
+            self.measurement._find_least_latent_url(self.example_urls),
+            [(self.example_urls[1], results[1]), (self.example_urls[2], results[2]), (self.example_urls[0], results[0])]
+        )
+
+    @mock.patch.object(DownloadSpeedMeasurement, "_get_latency_results")
+    def test_sort_one_url(self, mock_latency_results):
+        results = [
+            LatencyMeasurementResult(
+                id="test",
+                host="n2-validfakehost.com",
+                minimum_latency=None,
+                average_latency=25.0,
+                maximum_latency=None,
+                median_deviation=None,
+                errors=[],
+            ),
+        ]
+        mock_latency_results.side_effect = results
+        self.assertEqual(
+            self.measurement._find_least_latent_url([self.example_urls[1]]),
+            [(self.example_urls[1], results[0])]
+        )
